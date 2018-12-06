c36af2b07409ecf55c36f220e441047ea95d2333
Unknown <steven.washington.2021@student.lhschools.org>
2018-12-06 13:24:59 -0500

    Changes in IntelliJ

---
+++ src/Hangman.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Hangman {
+
+    public static void main(String[] args){
+        Scanner keyboard;
+
+        //initalizing
+        String Player1;
+        String Player2;
+        String letter;
+        String chosenword;
+        String again;
+        String Incorrectphrase;
+
+
+
+        keyboard = new Scanner(System.in);
+
+        System.out.println("Hello there and WELCOME to the Hangman Game");
+        System.out.println("Here are the rules, you can guess as many times as you want but you have 7 mistakes.");
+        System.out.println("If you guess incorrectly and go over 7 mistakes, oyu get it wrong.");
+        System.out.println("Lets Begin!");
+
+
+        for (int x=0; x<=15;x++){
+        }
+
+        System.out.println("+------------+");
+        System.out.println("|            |");
+        System.out.println("|            |");
+        System.out.println("|            |");
+        System.out.println("|            |");
+        System.out.println("+------------+");
+    }
+}
